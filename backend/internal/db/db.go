676905f1ec1a3c77780eb6010d87b7adeb79f78e
Chaitanya Bankanhal <chaitanya@uptiq.ai>
2026-05-01 19:03:48 +0530

    Add auto migrate

--- backend/internal/db/db.go
+++ backend/internal/db/db.go
@@ -19,11 +19,8 @@ func Setup(cfg config.Config) (*gorm.DB, error) {
 
 	slog.Info("connected to database", "dsn", cfg.Postgres.DSN)
 
-	if cfg.Env == "local" {
-		// Run migrations
-		if err := AutoMigrate(db); err != nil {
-			return nil, fmt.Errorf("failed to run migrations: %w", err)
-		}
+	if err := AutoMigrate(db); err != nil {
+		return nil, fmt.Errorf("failed to run migrations: %w", err)
 	}
 
 	return db, nil
